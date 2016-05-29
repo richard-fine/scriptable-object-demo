4add2cf332cffdfc67e43b8b76a383fd4dd737ce
Richard Fine <richardf@unity3d.com>
2016-05-29 12:29:37 +0100

    Add missing CompositeAudioEvent class

---
+++ Assets/ScriptableObject/Audio/CompositeAudioEvent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[CreateAssetMenu(menuName="Audio Events/Composite")]
+public class CompositeAudioEvent : AudioEvent
+{
+	[Serializable]
+	public struct CompositeEntry
+	{
+		public AudioEvent Event;
+		public float Weight;
+	}
+
+	public CompositeEntry[] Entries;
+
+	public override void Play(AudioSource source)
+	{
+		float totalWeight = 0;
+		for (int i = 0; i < Entries.Length; ++i)
+			totalWeight += Entries[i].Weight;
+
+		float pick = Random.Range(0, totalWeight);
+		for (int i = 0; i < Entries.Length; ++i)
+		{
+			if (pick > Entries[i].Weight)
+			{
+				pick -= Entries[i].Weight;
+				continue;
+			}
+
+			Entries[i].Event.Play(source);
+			return;
+		}
+	}
+}
