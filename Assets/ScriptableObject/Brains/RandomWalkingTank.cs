7c0a8ed944dbdfd0f1e6d4439cb646582c9478a3
Richard Fine <richardf@unity3d.com>
2016-05-29 12:42:11 +0100

    Add Idiot and Sniper brains

---
+++ Assets/ScriptableObject/Brains/RandomWalkingTank.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(menuName="Brains/Random walker")]
+public class RandomWalkingTank : TankBrain
+{
+	[MinMaxRange(0, 10)]
+	public RangedFloat idleTime;
+	[MinMaxRange(0, 10)]
+	public RangedFloat moveTime;
+	[MinMaxRange(0, 10)]
+	public RangedFloat fireTime;
+
+	public enum States
+	{
+		Idle,
+		Moving,
+		Firing
+	}
+
+	public override void Initialize(TankThinker tank)
+	{
+		tank.Remember("state", States.Idle);
+		tank.Remember("stateTimeout", Random.Range(idleTime.minValue, idleTime.maxValue));
+	}
+
+	public override void Think(TankThinker tank)
+	{
+		float stateTimeout = tank.Remember<float>("stateTimeout");
+		stateTimeout -= Time.deltaTime;
+		tank.Remember("stateTimeout", stateTimeout);
+
+		var state = tank.Remember<States>("state");
+
+		var move = tank.GetComponent<TankMovement>();
+		if (state == States.Moving)
+		{
+			move.Steer(tank.Remember<float>("moveForwardBack"), tank.Remember<float>("moveLeftRight"));
+		}
+		else
+		{
+			move.Steer(0, 0);
+		}
+
+		if (stateTimeout < 0)
+		{
+			switch (state)
+			{
+				case States.Idle:
+				{
+					tank.Remember("state", States.Moving);
+					tank.Remember("stateTimeout", Random.Range(moveTime.minValue, moveTime.maxValue));
+					tank.Remember("moveForwardBack", Random.Range(-1f, 1f));
+					tank.Remember("moveLeftRight", Random.Range(-1f, 1f));
+					break;
+				}
+				case States.Moving:
+				{
+					tank.Remember("state", States.Firing);
+					tank.Remember("stateTimeout", Random.Range(fireTime.minValue, fireTime.maxValue));
+
+					var tankFire = tank.GetComponent<TankShooting>();
+					tankFire.BeginChargingShot();
+
+					break;
+				}
+				case States.Firing:
+				{
+					var tankFire = tank.GetComponent<TankShooting>();
+					tankFire.FireChargedShot();
+
+					tank.Remember("state", States.Idle);
+					tank.Remember("stateTimeout", Random.Range(idleTime.minValue, idleTime.maxValue));
+
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/ScriptableObject/Brains/SimpleSniper.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+[CreateAssetMenu(menuName="Brains/Simple sniper")]
+public class SimpleSniper : TankBrain
+{
+
+	public float aimAngleThreshold = 2f;
+	[MinMaxRange(0, 0.05f)]
+	public RangedFloat chargeTimePerDistance;
+	[MinMaxRange(0, 10)]
+	public RangedFloat timeBetweenShots;
+
+	public override void Think(TankThinker tank)
+	{
+		GameObject target = tank.Remember<GameObject>("target");
+		var movement = tank.GetComponent<TankMovement>();
+
+		if (!target)
+		{
+			// Find the nearest tank that isn't me
+			target =
+				GameObject
+					.FindGameObjectsWithTag("Player")
+					.OrderBy(go => Vector3.Distance(go.transform.position, tank.transform.position))
+					.FirstOrDefault(go => go != tank.gameObject);
+
+			tank.Remember<GameObject>("target");
+		}
+
+		if (!target)
+		{
+			// No targets left - drive in a victory circles
+			movement.Steer(0.5f, 1f);
+			return;
+		}
+
+		// aim at the target
+		Vector3 desiredForward = (target.transform.position - tank.transform.position).normalized;
+		if (Vector3.Angle(desiredForward, tank.transform.forward) > aimAngleThreshold)
+		{
+			bool clockwise = Vector3.Cross(desiredForward, tank.transform.forward).y > 0;
+			movement.Steer(0f, clockwise ? -1 : 1);
+		}
+		else
+		{
+			// Stop
+			movement.Steer(0f, 0f);
+		}
+
+		// Fire at the target
+		var shooting = tank.GetComponent<TankShooting>();
+		if (!shooting.IsCharging)
+		{
+			if (Time.time > tank.Remember<float>("nextShotAllowedAfter"))
+			{
+				float distanceToTarget = Vector3.Distance(target.transform.position, tank.transform.position);
+				float timeToCharge = distanceToTarget*Random.Range(chargeTimePerDistance.minValue, chargeTimePerDistance.maxValue);
+				tank.Remember("fireAt", Time.time + timeToCharge);
+				shooting.BeginChargingShot();
+			}
+		}
+		else
+		{
+			float fireAt = tank.Remember<float>("fireAt");
+			if (Time.time > fireAt)
+			{
+				shooting.FireChargedShot();
+				tank.Remember("nextShotAllowedAfter", Time.time + Random.Range(timeBetweenShots.minValue, timeBetweenShots.maxValue));
+			}
+		}
+	}
+}
